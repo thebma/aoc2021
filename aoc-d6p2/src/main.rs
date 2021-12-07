ee62c3cafd3a8f31d14459c10d99ce7dc7120ff9
thebma <boum.dev@protonmail.com>
2021-12-07 12:15:22 +0100

    removed commented code

--- aoc-d6p2/src/main.rs
+++ aoc-d6p2/src/main.rs
@@ -52,37 +52,5 @@ fn main()
         sum += all;
     }
 
-    println!("{}", sum);
-
-
-    // let days = 256u32;
-
-    // for day in 0..days
-    // {
-    //     let len = school.len();
-
-    //     let dec_school = school
-    //         .clone()
-    //         .into_iter()
-    //         .map(|x| x - 1)
-    //         .collect::<Vec<i32>>();
-
-    //     let mut new_school: Vec<i32> = Vec::new();
-
-    //     for value in dec_school 
-    //     {
-    //         if value == -1 {
-    //             new_school.push(6);
-    //             new_school.push(8);
-    //         }
-    //         else {
-    //             new_school.push(value)
-    //         }
-    //     }
-
-    //     school = new_school;
-    //     println!("Day {} \t {}", day, school.len());
-    // }
-
-    //println!("School size is {}", school.len());
+    println!("Total amount of fish is {}", sum);
 }
